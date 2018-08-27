0c886b98d5366a6d347ee26c2d952a957074cf20
Dylan Alcazar <dylan.alcazar@gmail.com>
2018-08-26 20:25:59 -0600

    Going to add JWT

---
+++ CRUD_Server/Controllers/PaymentsController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CRUD_Server.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRUD_Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentsController : ControllerBase
+    {
+        ApplicationContext _context;
+
+        public PaymentsController(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Payment>> GetAll()
+        {
+            return _context.Payments.ToList();
+        }
+
+        [HttpGet("{id}", Name ="Getpayment")]
+        public ActionResult<Payment> GetById(long id)
+        {
+            var item = _context.Payments.Find(id);
+            if (item == null)
+                return NotFound();
+
+            return Ok(item);
+        }
+
+        [HttpPost]
+        public IActionResult Create(Payment item)
+        {
+            if (!FoundBankAccount(item.BankAccount))
+                return NotFound("This bank account does not exist");
+
+            return CreatedAtRoute("Getpayment", new { id = item.Id }, item);
+
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(long id, Payment item)
+        {
+            var payment = _context.Payments.Find(id);
+            if (payment == null)
+                return NotFound();
+
+            payment.PaymentStatus = item.PaymentStatus;
+
+            _context.Payments.Update(payment);
+            _context.SaveChanges();
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+            var item = _context.Payments.Find(id);
+            if (item == null)
+                return NotFound();
+            _context.Payments.Remove(item);
+            _context.SaveChanges();
+            return Ok();
+        }
+
+        private bool FoundBankAccount(long bankAccount)
+        {
+            var item = _context.BankAccounts.Find(bankAccount);
+            if (item == null)
+                return false;
+
+            return true;
+        }
+    }
+}
---
+++ CRUD_Server/Controllers/TransfersController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CRUD_Server.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRUD_Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransfersController : ControllerBase
+    {
+        ApplicationContext _context;
+
+        public TransfersController(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Transfer>> GetAll()
+        {
+            return _context.Transfers.ToList();
+        }
+
+        [HttpGet("{id}", Name ="Gettransfer")]
+        public ActionResult<Transfer> GetById(long id)
+        {
+            var item = _context.Transfers.Find(id);
+            if (item == null)
+                return NotFound();
+
+            return Ok(item);
+        }
+
+        [HttpPost]
+        public IActionResult Create(Transfer item)
+        {
+            if (!FoundBankAccount(item.BankAccount))
+                return NotFound();
+
+            _context.Transfers.Add(item);
+            _context.SaveChanges();
+            return CreatedAtRoute("Gettransfer", new { id = item.Id }, item);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(long id, Transfer item)
+        {
+            var transfer = _context.Transfers.Find(id);
+            if (transfer == null)
+                return NotFound();
+
+            transfer.Amount = item.Amount;
+
+            _context.Transfers.Add(transfer);
+            _context.SaveChanges();
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+            var transfer = _context.Transfers.Find(id);
+            if (transfer == null)
+                return NotFound();
+
+            _context.Transfers.Remove(transfer);
+            _context.SaveChanges();
+            return Ok();
+        }
+
+        private bool FoundBankAccount(long bankAccount)
+        {
+            var item = _context.BankAccounts.Find(bankAccount);
+            if (item == null)
+                return false;
+
+            return true;
+        }
+    }
+}
